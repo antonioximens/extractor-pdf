383e44cdb55a4b4eb735c054c39e1341300c728e
antonioximens <almeid.ximenes@gmail.com>
2026-04-27 22:49:46 -0400

    Fix:Correcting the next.config file v1

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
       canvas: "./src/lib/empty-module.js",
     },
   },
+  // Suprime warnings de config inválida
+  reactStrictMode: true,
 };
 
 export default nextConfig;
